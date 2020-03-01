65648b6df2f2be10e5793cf6a5cfbaab0946e0e6
Vitalii Chekalenko <43683151+VitaliiChekalenko@users.noreply.github.com>
2020-03-01 14:25:06 +0200

    changed direction of palyers tank

--- src/Game.java
+++ src/Game.java
@@ -20,17 +20,8 @@ public class Game implements Runnable {
     private Graphics2D graphics;
     private Input input;
     private TextureAtlas atlas;
-    private SpriteSheet sheet;
-    private Sprite sprite;
+    private Player player;
 
-
-    //temp
-    float x = 350;
-    float y = 250;
-    float delta = 0;
-    float radius = 50;
-    float speed = 3.0f;
-     //temp end
 
     public Game() {
         running = false;
@@ -39,8 +30,8 @@ public class Game implements Runnable {
         input = new Input();
         Display.addInputListener(input);
         atlas = new TextureAtlas(ATLAS_FILE_NAME);
-        sheet = new SpriteSheet(atlas.cut(8*16, 5*16, 16*2, 16 ), 2, 16);
-        sprite = new Sprite(sheet, 3);
+        player = new Player(300, 300, 3, 3, atlas);
+
     }
 
     public synchronized void start() {
@@ -66,23 +57,12 @@ public class Game implements Runnable {
     }
 
     private void update() {
-
-        if(input.getKey(KeyEvent.VK_UP)){
-            y -= speed;
-        }if(input.getKey(KeyEvent.VK_DOWN)){
-            y += speed;
-        }if(input.getKey(KeyEvent.VK_LEFT)){
-            x -= speed;
-        }if(input.getKey(KeyEvent.VK_RIGHT)){
-            x += speed;
-        }
-
-
-    }
+        player.update(input);
+            }
 
     private void render() {
         Display.clear();
-        sprite.render(graphics, x, y);
+        player.render(graphics);
         Display.swapBuffers();
     }
 
